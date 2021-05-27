69ced92c6a1fc7059cf2f45cb6572d48b1cfd844
planmi1287 <planmi1287@protonmail.com>
2021-05-27 14:50:16 +0100

    init

---
+++ server.js
@@ -0,0 +1,62 @@
+const express =  require('express')
+const app = express(); 
+const http = require('http').Server(app);
+const io = require('socket.io')(http)
+
+var data = {
+  paused: true,
+  currentTime: 0,
+  currentSource: undefined,
+}
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/admin', (req, res) => {
+  res.sendFile(__dirname + '/admin.html');
+});
+
+
+io.on('connection', (socket) => {
+
+    console.log('CLIENT CONNECTED (' + socket.id + ')');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        
+    });
+
+    socket.on('play video', () => {
+      console.log('PLAY MEDIA')
+      io.emit('play video');
+    });
+
+    socket.on('pause video', () => {
+      console.log('PAUSE MEDIA')
+      io.emit('pause video');
+    });
+
+
+    socket.on('sync video', () => {
+      console.log('SYNC TO A CLIENT (' + socket.id + ')')
+      console.log(data.currentTime)
+      socket.emit('set state', data.currentTime, data.currentSource, data.paused)
+    });
+
+
+    socket.on('return data', (time, src, paused) => {
+      data.currentTime = time
+      data.currentSource = src
+      data.paused = paused
+      
+      console.log(`CURRENT VIDEO STATE: ${data.currentTime} ${data.currentSource} ${data.paused}`)
+    });
+
+
+});
+
+http.listen(80, '192.168.86.178', () => {
+  console.log('listening on *:80');
+});
